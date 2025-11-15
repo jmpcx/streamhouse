527ea2904ca498c8a93ef944227d4005296f97a7
jmpcx <79660613+jmpcx@users.noreply.github.com>
2025-11-15 21:20:39 +0530

    Add API client and fetch movies in Home page
    Introduced axios and uuid dependencies. Added src/lib/apiClient.ts for API interactions and src/lib/networkUtil.ts for response typing. Updated Home.tsx to fetch movies using the new API client and refactored the layout. Updated package.json to include new dependencies.

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,154 @@
-﻿function Home()
-{
-    return (
-        <div className="container w-full h-full">
+﻿import SHClient from "@/lib/apiClient.ts";
 
+async function Home()
+{
+    const movies = await SHClient.getAllMovies();
+
+
+
+    return (
+        <div className="w-full h-full">
+            <div className='max-w-full'>
+                <div className='p-4 w-full h-full justify-items-center grid grid-cols-[repeat(auto-fill,_minmax(150px,_1fr))] gap-x-2 gap-y-4 sm:gap-x-4'
+                >
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <img alt='Tenet' className='h-[225px] w-[150px] bg-amber-300 rounded-2xl' src='https://m.media-amazon.com/images/M/MV5BYjI0NDQzYmEtNzMwZC00ODA3LTgzZDYtZTk5ODZjY2Y2OTkzXkEyXkFqcGc@._V1_.jpg' ></img>
+                        <div className='flex flex-col justify-center relative'>
+                            <h1 className='ml-2 pt-2 text-lg'>Tenet</h1>
+                            <p className='ml-2 text-sm text-muted-foreground absolute top-8'>2025</p>
+                            <div className='pb-2'></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
